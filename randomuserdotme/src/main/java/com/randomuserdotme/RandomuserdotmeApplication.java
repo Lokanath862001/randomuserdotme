97c9644fd77a90d9aba92f462533532c1253b0ab
LOKANATH SETHY <lokanath862001@gmail.com>
2023-11-27 16:33:24 +0530

    First Commit

---
+++ randomuserdotme/src/main/java/com/randomuserdotme/RandomuserdotmeApplication.java
@@ -0,0 +1,14 @@
+package com.randomuserdotme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomuserdotmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomuserdotmeApplication.class, args);
+		System.out.println("Spring Boot App RandomUserDotMe has Started Successfully!");
+	}
+
+}
---
+++ randomuserdotme/src/test/java/com/randomuserdotme/RandomuserdotmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomuserdotme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomuserdotmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
